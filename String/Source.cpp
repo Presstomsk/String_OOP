85d2e338d3b8dcabe625ac7c25ff093a9eea0c77
Roman <info@presstomsk.ru>
2021-04-25 14:32:21 +0700

    END

--- String/Source.cpp
+++ String/Source.cpp
@@ -110,9 +110,9 @@ String operator+(const String& left, const String& right)
 {
 	String result(left.get_size() + right.get_size() - 1);
 	for (int i = 0; i < left.get_size(); i++)
-		result.get_str()[i] = left.get_str()[i];
+		result.get_str()[i] = left[i];
 	for (int i = 0; i < right.get_size(); i++)
-		result.get_str()[i+left.get_size()-1] = right.get_str()[i];
+		result.get_str()[i+left.get_size()-1] = right[i];
 	return result;
 
 }
